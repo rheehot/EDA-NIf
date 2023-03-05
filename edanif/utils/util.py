b9ddafe807274be539cdfb2ee317d863e426b90a
parkminwoo <parkminwoo1991@gmail.com>
2023-03-05 22:58:41 +0900

    feat: util.py

---
+++ edanif/utils/util.py
@@ -0,0 +1,27 @@
+import argparse
+import pandas as pd
+import glob
+import os
+import nibabel as nib 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from io import StringIO
+
+
+def find_all_files(top_folder_path: str, include_word: str) -> list:
+    all_file_pathes = []
+    for (root, directories, files) in os.walk(top_folder_path):
+        for file in files:
+            if include_word in file:
+                detect_file_path = os.path.join(root, file)
+                all_file_pathes.append(detect_file_path)
+
+    return all_file_pathes
+    
+
+def save_print_instance(*message):
+    io = StringIO()
+    print(*message, file=io, end="")
+    return io.getvalue()
+
