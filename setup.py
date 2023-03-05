f1e24499d9230159b438c0c8f45691633534f8de
parkminwoo <parkminwoo1991@gmail.com>
2023-03-05 23:16:35 +0900

    build: 0.1.0

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edanif",
+    version="0.1.0",
+    author="parkminwoo",
+    author_email="parkminwoo1991@gmail.com",
+    description="EDA-NIf creates a dataframe containing meta information of NIfTi files and provides several useful features.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DSDanielPark/edanif",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        "nibabel",
+        "pandas",
+        "numpy",
+        "matplotlib"
+    ])
