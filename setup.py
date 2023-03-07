c73ac828116ab89b9934aa338d421df4f808b7c6
parkminwoo <parkminwoo1991@gmail.com>
2023-03-07 13:05:10 +0900

    setup: url changed

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="EDA-NIf creates a dataframe containing meta information of NIfTi files and provides several useful features.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DSDanielPark/edanif",
+    url="https://github.com/DSDanielPark/EDA-NIf",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=[
